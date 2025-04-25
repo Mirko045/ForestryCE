b376e37305090102340514a6661ac3635a7ea7da
Spearkiller <bradley56789@gmail.com>
2025-04-25 13:32:30 +1200

    Added a method to get a wood type from a log type

--- src/main/java/forestry/arboriculture/worldgen/TreeBlockTypeLog.java
+++ src/main/java/forestry/arboriculture/worldgen/TreeBlockTypeLog.java
@@ -36,4 +36,8 @@ public class TreeBlockTypeLog implements ITreeBlockType {
 	public boolean setBlock(LevelAccessor level, BlockPos pos) {
 		return this.tree.setLogBlock(this.genome, level, pos, this.facing);
 	}
+
+	public IGenome getGenome(){
+		return this.genome;
+	}
 }
