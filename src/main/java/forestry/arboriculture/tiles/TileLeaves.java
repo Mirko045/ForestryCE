932c67d5edc6cb2d001da16f65ac539e402fd52c
Spearkiller <bradley56789@gmail.com>
2025-04-27 18:04:59 +1200

    Overhauled mutations for Tree Breeding

--- src/main/java/forestry/arboriculture/tiles/TileLeaves.java
+++ src/main/java/forestry/arboriculture/tiles/TileLeaves.java
@@ -264,7 +264,7 @@ public class TileLeaves extends TileTreeContainer implements IFruitBearer, IButt
 	private int determineFruitColour() {
 		ITree tree = getTree();
 		if (tree == null) {
-			tree = SpeciesUtil.getTreeSpecies(ForestryTreeSpecies.HILL_CHERRY).createIndividual();
+			tree = SpeciesUtil.getTreeSpecies(ForestryTreeSpecies.SOUR_CHERRY).createIndividual();
 		}
 		IGenome genome = tree.getGenome();
 		IFruit fruit = genome.getActiveValue(TreeChromosomes.FRUIT);
